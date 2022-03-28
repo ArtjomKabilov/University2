84d3bb6e4c02d283ec74d61d7866328d960c8874
Artem Kabilov <artem1223148@gmail.com>
2022-03-28 20:30:06 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using University.DAL;
+using University.ViewModels;
+
+namespace University.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private SchoolContext db = new SchoolContext();
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+                return View(data.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
